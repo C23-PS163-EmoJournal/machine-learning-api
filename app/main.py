200be26f6c4552b8d1b5965d882198e60c35882b
Dimas Wahyu Saputro <73456370+dimaswsss@users.noreply.github.com>
2023-06-09 21:46:27 +0700

    Update main.py
    add create_upload_file

--- app/main.py
+++ app/main.py
@@ -1,11 +1,14 @@
 # import FastAPI
-from fastapi import FastAPI, UploadFile
+from fastapi import FastAPI, UploadFile, File
 from app.module import *
+import os
+import shutil
 
 app = FastAPI()
 
 @app.get("/files/{file_path:path}")
 def predict(file_path: str):
+    file_path = "/"+file_path
     emotion, confidence = prediction(file_path)
     
     return {
@@ -16,6 +19,19 @@ def predict(file_path: str):
 
 @app.post("/uploadfile/")
 async def create_upload_file(file: UploadFile):
+    upload_dir = os.path.join(os.getcwd(), "uploads")
+    # Create the upload directory if it doesn't exist
+    if not os.path.exists(upload_dir):
+        os.makedirs(upload_dir)
+
+    # get the destination path
+    dest = os.path.join(upload_dir, file.filename)
+    print(dest)
+
+    # copy the file contents
+    with open(dest, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
     return {"filename": file.filename}
 
 @app.get("/")

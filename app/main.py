d6b86ec0af99af9cac702c5e5175172b6f970e86
Dimas Wahyu Saputro <73456370+dimaswsss@users.noreply.github.com>
2023-06-14 19:32:33 +0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -3,12 +3,29 @@ from fastapi import FastAPI, UploadFile, File
 from app.module import *
 import os
 import shutil
+from google.cloud import storage
 
 app = FastAPI()
 
 @app.get("/predict/{file_path:path}")
 def predict(file_path: str):
     file_path = "/"+file_path
+    emotion, confidence = prediction(file_path)
+    
+    return {
+        "message": "200, Predict success",
+        "Emotion": emotion,
+        "Confidence": str(confidence)
+    }
+
+@app.get("/predict_bucket/{name_file}")
+def download_predict(name_file: str):
+    storage_client = storage.Client.create_anonymous_client()
+    bucket = storage_client.bucket("file-suara")
+    blob = bucket.blob(nama_file)
+    blob.download_to_filename("./files/")
+
+    file_path = "./files/"+name_file
     emotion, confidence = prediction(file_path)
     
     return {

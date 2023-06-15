107d6164e6a0aa86c140b739b2b4e555c7141c4c
Dimas Wahyu Saputro <73456370+dimaswsss@users.noreply.github.com>
2023-06-15 13:05:32 +0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -54,7 +54,7 @@ def download_predict(name: str):
 
 @app.post("/uploadfile/")
 async def create_upload_file(file: UploadFile):
-    upload_dir = os.path.join(os.getcwd(), "../code/downloads/")
+    upload_dir = os.path.join(os.getcwd(), "/downloads/")
     # Create the upload directory if it doesn't exist
     if not os.path.exists(upload_dir):
         os.makedirs(upload_dir)
@@ -73,7 +73,7 @@ async def create_upload_file(file: UploadFile):
 # dapatkan list file dari folder uploads
 @app.get("/list_files/")
 async def get_list_files():
-    upload_dir = os.path.join(os.getcwd(), "../code/downloads/")
+    upload_dir = os.path.join(os.getcwd(), "/downloads/")
     files = os.listdir(upload_dir)
 
     files = [str(i+1)+". "+files[i] for i in range(len(files))]
